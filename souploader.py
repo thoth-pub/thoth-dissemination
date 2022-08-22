4d2cc8a3cf5f60c8690503881fdc2dd5afc23411
rhigman <73792779+rhigman@users.noreply.github.com>
2022-08-22 15:09:00 +0100

    Basic program with one example each of FTP, SFTP and API upload

---
+++ souploader.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""
+Retrieve and disseminate files and metadata to ScienceOpen
+"""
+
+import logging
+import sys
+import pysftp
+import zipfile
+from datetime import date
+from io import BytesIO
+from os import environ
+from uploader import Uploader
+
+
+class SOUploader(Uploader):
+    """Dissemination logic for ScienceOpen"""
+
+
+    def upload_to_platform(self):
+        """
+        Upload work in required format to ScienceOpen.
+
+        Standard instructions:
+        - Create new directory, named with today's date (YYYY-MM-DD), in 'UPLOAD_TO_THIS_DIRECTORY'
+        - Add one zip file to this directory for each work in this upload batch
+        - Zip file should contain metadata and all relevant content files
+
+        Further details determined in discussion between Thoth and ScienceOpen:
+        - Create separate directories within upload directory for each publisher
+        - Supply work PDF, cover image, and metadata file (format TBD)
+        - Can continue existing pattern of using paperback ISBN for all filenames
+        - Chapter files and metadata required (full details TBD; not yet implemented)
+        """
+
+        publisher = self.get_publisher_name()
+        filename = self.get_pb_isbn()
+        root_dir = 'UPLOAD_TO_THIS_DIRECTORY'
+        new_dir = date.today().isoformat()
+
+        # Metadata file format TBD: use CSV for now
+        metadata_bytes = self.get_formatted_metadata('csv::thoth')
+        cover_bytes = self.get_cover_image()
+        pdf_bytes = self.get_pdf_bytes()
+
+        files = [
+            ('{}.csv'.format(filename), metadata_bytes),
+            ('{}.jpg'.format(filename), cover_bytes),
+            ('{}.pdf'.format(filename), pdf_bytes),
+        ]
+
+        zipped_files = BytesIO()
+        with zipfile.ZipFile(file=zipped_files, mode='w', compression=zipfile.ZIP_DEFLATED) as zf:
+            for f in files:
+                zf.writestr(zinfo_or_arcname=f[0], data=f[1])
+        # Reset buffer position to start of stream so that it can be fully read in upload
+        zipped_files.seek(0)
+
+        try:
+            with pysftp.Connection(
+                host='ftp.scienceopen.com',
+                username=environ.get('so_ftp_user'),
+                password=environ.get('so_ftp_pw'),
+            ) as sftp:
+                try:
+                    sftp.cwd(root_dir)
+                except FileNotFoundError:
+                    logging.error('Could not find folder UPLOAD_TO_THIS_DIRECTORY on ScienceOpen SFTP server')
+                    sys.exit(1)
+                try:
+                    sftp.cwd(publisher)
+                except FileNotFoundError:
+                    logging.error('Could not find folder for publisher "{}" on ScienceOpen SFTP server'.format(publisher))
+                    sys.exit(1)
+                sftp.mkdir(new_dir)
+                sftp.cwd(new_dir)
+                try:
+                    sftp.putfo(flo=zipped_files, remotepath='{}.zip'.format(filename))
+                except TypeError as error:
+                    logging.error('Error uploading to ScienceOpen SFTP server: {}'.format(error))
+                    sys.exit(1)
+        except pysftp.AuthenticationException as error:
+            logging.error('Could not connect to ScienceOpen SFTP server: {}'.format(error))
+            sys.exit(1)
+
+        logging.info('Successfully uploaded to ScienceOpen SFTP server')
+
+
+    def parse_metadata(self):
+        """Convert work metadata into ScienceOpen format"""
+        # Not required for ScienceOpen - only the metadata file is required
+        pass
