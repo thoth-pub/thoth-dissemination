b62eff17063ba12c83f8a1d9ba4e923dfc5a5a2a
rhigman <73792779+rhigman@users.noreply.github.com>
2023-01-16 14:44:17 +0000

    Review markups: replace hard-coded publishers list with basic script argument handling

--- obtain_new_ids.py
+++ obtain_new_ids.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 """
 Acquire a list of work IDs to be uploaded to Internet Archive.
-Purpose: automatic upload at regular intervals of newly-published OBP and punctum works.
+Purpose: automatic upload at regular intervals of newly-published works from selected publishers.
 Based on `iabulkupload/obtain_work_ids.py`.
 """
 
-# Both packages already included in thoth-dissemination/requirements.txt
+# Both third-party packages already included in thoth-dissemination/requirements.txt
 from internetarchive import search_items
-from thothlibrary import ThothClient
+from thothlibrary import errors, ThothClient
+import sys
 
 thoth = ThothClient()
 
-# Thoth IDs of Open Book Publishers and punctum respectively
-publishers = '["85fd969a-a16c-480b-b641-cb9adf979c3b", "9c41b13c-cecc-4f6a-a151-be4682915ef5"]'
+# Obtain IDs of publishers whose works should be uploaded from arguments to script
+# Test that all are valid and early-exit if not
+for publisher in sys.argv[1:]:
+    try:
+        thoth.publisher(publisher)
+    except errors.ThothError:
+        # Don't include full error text as it's lengthy (contains full query/response)
+        print("No record found for publisher {}: ID may be incorrect".format(publisher))
+        sys.exit(1)
 
-# Obtain all active (published) works listed in Thoth from OBP and punctum.
+publishers = '["' + '", "'.join(sys.argv[1:]) + '"]'
+
+# Obtain all active (published) works listed in Thoth from the selected publishers.
 # `books` query includes Monographs, Edited Books, Textbooks and Journal Issues
 # but excludes Chapters and Book Sets.
 thoth_works = thoth.books(
@@ -32,7 +42,7 @@ thoth_ids = [n.workId for n in thoth_works]
 # Obtain all works listed in the Internet Archive's Thoth Archiving Network collection.
 # We only need the identifier; this matches the Thoth work ID.
 # If the collection later grows to include more publishers, we may want to
-# additionally filter the query to only return OBP and punctum works.
+# additionally filter the query to only return works from those selected.
 ia_works = search_items(
     query='collection:thoth-archiving-network', fields=['identifier'])
 
@@ -40,7 +50,7 @@ ia_works = search_items(
 ia_ids = [n['identifier'] for n in ia_works]
 
 # The set of IDs of works that need to be uploaded to the Internet Archive
-# is those which appear as published for OBP/punctum in Thoth
+# is those which appear as published for the selected publishers in Thoth
 # but do not appear as already uploaded to the IA collection.
 # Note that if any work has been determined as ineligible for upload
 # (e.g. due to not being available as a PDF), it will continue to appear
