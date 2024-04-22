f0aff88c8f7e0660e852812004319e38f029e4aa
rhigman <73792779+rhigman@users.noreply.github.com>
2024-04-22 11:29:00 +0100

    Alternative taking file as input (compatible with passing artifacts in Actions)

--- write_locations.py
+++ write_locations.py
@@ -29,9 +29,8 @@ def write_thoth_location(publication_id, location_platform, landing_page, full_t
     thoth.create_location(location)
 
 if __name__ == '__main__':
-    args = sys.argv
-    publication_id = args[1]
-    location_platform = args[2]
-    landing_page = args[3]
-    full_text_url = args[4]
-    write_thoth_location(publication_id, location_platform, landing_page, full_text_url)
+    locations_file = sys.argv[1]
+    with open(locations_file, 'r') as locations:
+        for location in locations:
+            parts = location.rstrip().split(' ')
+            write_thoth_location(parts[0], parts[1], parts[2], parts[3])
