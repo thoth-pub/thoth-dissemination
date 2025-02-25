6cd133cd7daf7a06d981b0b890a49c32901a75aa
rhigman <73792779+rhigman@users.noreply.github.com>
2025-02-25 17:13:32 +0000

    Also obtain DOAB locations (including updating write_locations to handle None full text URLs)

--- obtain_locations.py
+++ obtain_locations.py
@@ -34,4 +34,21 @@ for (publication_id, doi) in works_to_search:
     except IndexError:
         logging.info('No results found in OAPEN for {} - assume not yet processed'.format(doi))
 
+    # Assume that if OAPEN location is missing, DOAB location is too
+    doab_rsp = requests.get(
+        url='https://directory.doabooks.org/rest/search?query=oapen.identifier.doi:%22{}%22' \
+            '&expand=metadata'
+            .format(doi),
+        headers={'Accept': 'application/json'},
+    )
+    try:
+        doab_rsp_json = json.loads(doab_rsp.content)[0]
+        handle = doab_rsp_json.get('handle')
+        # DOAB only has landing pages, not full text URLs
+        doab_landing_page = 'https://directory.doabooks.org/handle/{}'.format(handle)
+        logging.info('{} has DOAB landing page {}'.format(doi, doab_landing_page))
+        locations.append('{} DOAB {} {}'.format(publication_id, doab_landing_page, None))
+    except IndexError:
+        logging.info('No results found in DOAB for {} - assume not yet processed'.format(doi))
+
 print(json.dumps(locations))
