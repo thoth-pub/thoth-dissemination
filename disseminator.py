af1074b16f924b2a2f3ae9ddbc891d06a4afee93
Brendan O'Connell <brendan@openbookpublishers.com>
2025-08-13 11:13:40 +0200

    Update changelog, bump version number in disseminator.py

--- disseminator.py
+++ disseminator.py
@@ -6,7 +6,7 @@ Call custom workflows to retrieve work-related files and metadata
 and upload them in the appropriate format to various platforms.
 """
 
-__version__ = '0.1.30'
+__version__ = '0.1.31'
 
 import argparse
 import logging
