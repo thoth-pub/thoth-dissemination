2b1225651c4d5dcb2aa2e596b1fca3873805495e
rhigman <73792779+rhigman@users.noreply.github.com>
2025-11-13 17:09:23 +0000

    OAPEN dev SWORD server URL has changed

--- oapensworduploader.py
+++ oapensworduploader.py
@@ -15,8 +15,8 @@ class OAPENSWORDUploader(DSpaceUploader):
         """Set OAPEN-specific parameters and pass them to DSpaceUploader"""
         user_name_string = 'oapen_sword_user'
         user_pass_string = 'oapen_sword_pw'
-        service_document_iri = ("https://admin.oapen-dev.siscern.org/swordv2/servicedocument")
-        collection_iri = ("https://admin.oapen-dev.siscern.org/swordv2/collection/20.500.12657/97099")
+        service_document_iri = ("https://oapen-dev.siscern.org/swordv2/servicedocument")
+        collection_iri = ("https://oapen-dev.siscern.org/swordv2/collection/20.500.12657/97099")
         metadata_profile = MetadataProfile.OAPEN
         super().__init__(
             work_id,
